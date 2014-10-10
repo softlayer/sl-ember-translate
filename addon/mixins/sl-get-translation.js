54a17578bf08bcf49bdaf422564c9e9a652e314d
Jeremy Brown <jebrown@softlayer.com>
2014-10-10 09:22:39 -0500

    Add comments

--- addon/mixins/sl-get-translation.js
+++ addon/mixins/sl-get-translation.js
@@ -2,6 +2,14 @@ import Ember from 'ember';
 
 /** @module sl-translate/mixins/sl-get-translation */
 export default Ember.Mixin.create({
+
+    /**
+     * Based on value of key, retrieve translation or usual get() value
+     *
+     * @function get
+     * @argument {string} key property to retrieve
+     * @return   {string}
+     */
     get: function( key ) {
         var translationsRegex = /translate\.(.*)/,
             matches           = key.match( translationsRegex );
@@ -9,6 +17,13 @@ export default Ember.Mixin.create({
         return ( matches ) ? this.translate( matches[1] ) : this._super( key );
     },
 
+    /**
+     * Retrieve translated key without support for token replacement or pluralization
+     *
+     * @function translate
+     * @argument {text} key key to translate
+     * @return   {text}     translated key
+     */
     translate: function( key ) {
         return this.translateService.getKeyValue( key );
     }
