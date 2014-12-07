ca6c7bca80b6733908054d124e9bb77bbdee3c0d
Jeremy Brown <jeremy@notmessenger.com>
2014-12-07 11:38:35 -0600

    Move HTML to correct file; set groundwork for content-for to be dynamically populated

--- index.js
+++ index.js
@@ -2,5 +2,21 @@
 'use strict';
 
 module.exports = {
-  name: 'sl-ember-translate'
+  name: 'sl-ember-translate',
+
+  contentFor: function( type, config ) {
+    var content;
+
+    switch ( type ) {
+        case 'keywords':
+            content = 'ember, ember cli, translate, translation, dictionary';
+            break;
+
+        case 'description':
+            content = 'An Ember CLI Addon that provides the ability to lookup a dictionary value through method calls or via a component in your templates.';
+            break;
+    }
+
+    return content;
+  }
 };
