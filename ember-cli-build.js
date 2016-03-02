93d6fcdecacc69e0a3789a185f865309fecb663d
Jeremy Brown <jeremy@notmessenger.com>
2016-03-01 19:32:06 -0600

    Closes softlayer/sl-ember-translate#229

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,15 +1,16 @@
+/* jshint node:true */
 /* global require, module */
-const EmberApp = require( 'ember-cli/lib/broccoli/ember-addon' );
+const EmberAddon = require( 'ember-cli/lib/broccoli/ember-addon' );
 const replace = require( 'broccoli-string-replace' );
 const packageConfig = require( './package.json' );
 
 module.exports = function( defaults ) {
-    const app = new EmberApp( defaults, {
+    const app = new EmberAddon( defaults, {
         // Add options here
     });
 
     /*
-    This build file specifes the options for the dummy test app of this
+    This build file specifies the options for the dummy test app of this
     addon, located in `/tests/dummy`
     This build file does *not* influence how the addon or the app using it
     behave. You most likely want to be modifying `./index.js` or app's build file
